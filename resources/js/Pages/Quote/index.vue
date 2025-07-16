3ca3d893d904123606f91c6e1bcd892c5e05c28b
Giang Nguyễn <giangnkt.official@gmail.com>
2025-07-16 15:00:39 +0700

    change layout

---
+++ resources/js/Pages/Quote/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+</script>
+<template>
+    <AppLayout title="Quotes">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Quotes
+            </h2>
+            
+        </template>
+        <div class="grid grid-cols-12 gap-4 mx-3 my-5">
+                <div class="col-span-2 glassmorphism">01</div>
+                <!-- ... -->
+                <div class="col-span-10 glassmorphism">09</div>
+            </div>
+    </AppLayout>
+</template>
+<style scoped>
+h1 {
+    color: #333;
+}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+     Route::get('/quote', function () {
+        return Inertia::render('Quote/index');
+    })->name('quote');
 });
